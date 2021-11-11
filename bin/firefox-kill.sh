bfc8790354dbaa7a1a129a479a3038e8c4213aca
heow <heow@alphageeksinc.com>
2021-11-11 14:58:14 -0500

    updates

--- bin/firefox-kill.sh
+++ bin/firefox-kill.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-killall firefox firefox-bin run-mozilla.sh
+killall firefox firefox-bin run-mozilla.sh /usr/lib/firefox/firefox
 find ~/.mozilla/firefox -name .parentlock -exec rm {} \;
 find ~/.mozilla/firefox -name lock -exec rm {} \;
--- bin/osx-dc-restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "restarting OSX DoubleCommand system service..."
-sudo /Library/StartupItems/DoubleCommand/DoubleCommand stop
-sudo /Library/StartupItems/DoubleCommand/DoubleCommand start
