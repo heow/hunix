9d9fc78f04c49fe6485950e3332cd01f2457ef46
heow <heow@alphageeksinc.com>
2021-11-11 15:55:43 -0500

    minor enhancements

--- bin/restart-sh.sh
+++ bin/restart-sh.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+if [ -z "$2" ] ; then
+    echo "usage restart.sh 4 foo"
+    echo "  every 4 seconds run foo"
+    exit
+fi
 while true; do
   echo -n "process started: "; date
-  $2
+  $2 $3 $4 $5 $6
   sleep $1
   echo -n "process stopped: "; date
 done
