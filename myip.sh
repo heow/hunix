93194037b367967df7c5455ba576001da8a148c0
Heowbert <heow@heow.net>
2025-01-14 10:40:13 -0500

    ignore docker subnet

--- myip.sh
+++ myip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 namestr=`uname`
 if [[ "$namestr" == 'Linux' ]]; then
-    hostname -I | cut -f 1 -d " "
+    ip a | grep -v docker0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'    
+    #hostname -I | cut -f 1 -d " "
     #ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
     exit
 fi
